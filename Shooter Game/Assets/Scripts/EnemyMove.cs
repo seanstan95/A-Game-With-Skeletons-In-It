a0ba93b55f0405e23231b4eea7a232c65bf744e3
Sean <seanstan95@gmail.com>
2017-10-15 03:47:19 -0400

    Fixes - Camera is locked at 45 degree angle similar to Roll-a-Ball Tutorial - Player will rotate via mouse position on the ground, similar to Survival Shooter Tutorial - Enemy now properly follows the player

---
+++ Shooter Game/Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+	Transform player;
+	//PlayerHealth playerHealth;
+	//EnemyHealth enemyHealth;
+	UnityEngine.AI.NavMeshAgent nav;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		//playerHealth = player.GetComponent<playerHealth> ();
+		//enemyHealth = GetComponent<enemyHealth> ();
+		nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+	}
+
+	void Update()
+	{
+		//if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0) {
+			nav.SetDestination (player.position);
+		//} else {
+		//	nav.enabled = false;
+		//}
+	}
+}
---
+++ Shooter Game/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+	public float speed = 6f;
+	Vector3 movement;
+	Animator anim;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+
+	void Awake()
+	{
+		floorMask = LayerMask.GetMask ("Floor");
+		anim = GetComponent<Animator>();
+		playerRigidbody = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate()
+	{
+		float h = Input.GetAxisRaw ("Horizontal");
+		float v = Input.GetAxisRaw ("Vertical");
+
+		Move (h, v);
+		Turning ();
+		Animate (h, v);
+	}
+
+	void Move(float h, float v)
+	{
+		movement.Set (h, 0f, v);
+		movement = movement.normalized * speed * Time.deltaTime;
+		playerRigidbody.MovePosition (transform.position + movement);
+	}
+
+	void Turning()
+	{
+		Ray camRay = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		RaycastHit floorhit;
+
+		if (Physics.Raycast (camRay, out floorhit, camRayLength, floorMask)) {
+			Vector3 playerToMouse = floorhit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation (playerToMouse);
+			playerRigidbody.MoveRotation (newRotation);
+		}
+	}
+
+	void Animate(float h, float v)
+	{
+		bool walking = h != 0f || v != 0f;
+		anim.SetBool ("IsWalking", walking);
+	}
+}
