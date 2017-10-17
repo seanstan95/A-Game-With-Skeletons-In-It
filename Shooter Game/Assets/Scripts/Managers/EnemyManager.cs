70b11633ffa14c444a2f58599909a39a6c8cf2cc
Sean <seanstan95@gmail.com>
2017-10-17 01:28:12 -0400

    Recreating Debug Room - WORK IN PROGRESS: Buggy still

---
+++ Shooter Game/Assets/Scripts/Managers/EnemyManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour {
+
+	public PlayerHealth playerHealth;
+	public GameObject skeleton;
+	public float spawnTime = 3f;
+	public Transform[] spawnPoints;
+
+	void Start()
+	{
+		InvokeRepeating ("Spawn", spawnTime, spawnTime);
+	}
+
+	void Spawn()
+	{
+		if (playerHealth.currentHealth <= 0f) {
+			return;
+		}
+
+		int spawnPointIndex = Random.Range (0, spawnPoints.Length);
+		Instantiate (skeleton, spawnPoints [spawnPointIndex].position, spawnPoints [spawnPointIndex].rotation);
+	}
+}
