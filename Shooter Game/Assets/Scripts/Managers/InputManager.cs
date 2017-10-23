9e94d47bf99aa8a57bfe78ff100755547894ea50
Sean <seanstan95@gmail.com>
2017-10-23 02:31:30 -0400

    Main Menu

---
+++ Shooter Game/Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InputManager : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape))
+			Application.Quit ();
+		if (Input.GetKeyDown (KeyCode.Space))
+			Debug.Log ("Insert Pause Menu Here");//pause menu
+	}
+}
